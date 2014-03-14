52869f81dad46c84aac272718c935d573b565748
Tim Medina <iamteem@gmail.com>
2014-03-14 12:51:02 +0800

    Remove unnecessary instance_eval.

--- lib/hairy_dangerzone.rb
+++ lib/hairy_dangerzone.rb
@@ -8,18 +8,16 @@ module HairyDangerzone
 
     module ClassMethods
       def danger_danger(attribute, fail_message, &block)
-        self.instance_eval do
-          define_method :"#{attribute}_with_danger_danger=" do |new_value|
-            if block.call(new_value)
-              send(:"#{attribute}_without_danger_danger=", new_value)
-            else
-              fail fail_message
-            end
+        define_method :"#{attribute}_with_danger_danger=" do |new_value|
+          if block.call(new_value)
+            send(:"#{attribute}_without_danger_danger=", new_value)
+          else
+            fail fail_message
           end
-
-          alias_method :"#{attribute}_without_danger_danger=", :"#{attribute}="
-          alias_method :"#{attribute}=", :"#{attribute}_with_danger_danger="
         end
+
+        alias_method :"#{attribute}_without_danger_danger=", :"#{attribute}="
+        alias_method :"#{attribute}=", :"#{attribute}_with_danger_danger="
       end
     end
   end
